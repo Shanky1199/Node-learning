3085411233f7f492d5ce1a2f140d682d7114ad24
Shanky1199 <shashank.is17@sahyadri.edu.in>
2021-08-26 08:06:17 +0530

    adding the practice task with web api

---
+++ Extras/practice/app.js
@@ -0,0 +1,20 @@
+const axios = require('axios');
+
+async function getData(){
+    try{
+    var res = await axios('https://reqres.in/api/users')
+    //console.log(res.data.data)
+    return res.data.data
+
+    }catch{
+        console.log("err")
+        return null 
+    }
+    
+
+}
+
+
+
+
+module.exports = getData
